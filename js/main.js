e5d8b127e2291ef8c9f92830ae1f97156330beb7
AtanoDas <76824764+AtanoDas@users.noreply.github.com>
2021-08-03 05:00:29 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,35 @@
- var eye = document.getElementById('eye');
+/*Header js____*/
 
- eye.addEventListener('click', function(eye){
- 	eye.target.classList.toggle('eye-open');
-
-
-
- 	})
+window.addEventListener("scroll", function(){
+  var header =document.querySelector("header");
+  heade.classList.toggle("sticky",window.scrollY > 0);
+})
 
 
+
+/*eye button js*/
+
+var state = false;
+
+function toggle(){
+  if(state){
+    document.getElementById("password").setAttribute("type","password");
+    document.getElementById("eye-img").src="img/icons8-invisible-96.png";
+    state = false;
+  }
+  else{
+    document.getElementById("password").setAttribute("type","text");
+    document.getElementById("eye-img").src="img/icons8-eye-96.png";
+
+    state = true;
+
+
+  }
+}
+
+ 
+
+/*Remember icon js*/
   var remember = document.getElementById('rememberid');
 
  remember.addEventListener('click', function(remember){
